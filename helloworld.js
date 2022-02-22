ca15e8e5bfe5769e7f208f05ec4b0a429f56e4e5
Viniboyfull <leocadio.viny@gmail.com>
2022-02-21 22:35:10 -0300

    1.0

---
+++ helloworld.js
@@ -0,0 +1,8 @@
+console.log('Hello World')
+var a = 2;
+console.log('Esse é o conteúdo de a = ',a);
+var b = 3;
+console.log('Esse é o resultado de ', a , '*' , b , '=', a*b)
+let nome = 'Vini'
+console.log('Meu nome é', nome)
+console.log(nome * a) // Results in "Not a Number" (NaN)
